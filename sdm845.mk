fe461328985d2aca3d15a2a64e86e575f140b251
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-03 23:21:11 +0530

    sdm845-common: Speed preopt SysUI/Launcher/Settings
    Change-Id: I5abc0ecd7611127a8b43f28d0663677f625785e3

--- sdm845.mk
+++ sdm845.mk
@@ -21,6 +21,11 @@ include build/make/target/product/iorap_large_memory_config.mk
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    Settings \
+    SystemUI
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
