48ca12a05e6071cdc8140b3cc837d73d62d37155
notakbuzz <bistodroid@gmail.com>
2021-10-08 10:45:17 +0000

    [DNM] sdm845-common: disable ifaa as of now

--- sdm845.mk
+++ sdm845.mk
@@ -197,11 +197,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_PACKAGES += \
+#    org.ifaa.android.manager
+#
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # IMS
 PRODUCT_PACKAGES += \
