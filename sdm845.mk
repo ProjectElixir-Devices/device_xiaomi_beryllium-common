94e7913c22d733afc0e5a5acb113f0da0d59f9b8
johnmart19 <johnivan19999@gmail.com>
2022-09-03 13:51:04 +0300

    sdm845-common: Build missing Soundtrigger 2.0
    [  641.029356] init: Control message: Could not find 'android.hardware.soundtrigger@2.0::ISoundTriggerHw/default' for ctl.interface_start from pid: 631 (/system/bin/hwservicemanager)

--- sdm845.mk
+++ sdm845.mk
@@ -67,6 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.r_submix.default \
     audio.usb.default \
