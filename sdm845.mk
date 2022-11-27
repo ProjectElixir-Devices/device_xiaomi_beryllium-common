8e685f01cdf944dbed2d45696910ff4ee497cf1f
Cykeek <Cykeek@proton.me>
2022-11-27 22:55:57 +0530

    sdm845-common: build libgui_vendor  - dependency of displayfeature.default.so
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- sdm845.mk
+++ sdm845.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor
 
+# Libgui
+PRODUCT_PACKAGES += \
+    libgui_vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
